b0c19e3f78fb8c94cc0de4cede1a09816fbc4756
kevin <zhizhong.qiu@happyelements.com>
2012-02-13 16:45:23 +0800

    update pages

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 git pull;mvn clean package;
+hehwi=/home/hadoop/hehwi
+cp -f $hehwi/target/`ls $hehwi/target |grep jar` /home/hadoop/hive/lib/hive.web.jar
 netstat -ntpl |grep 9999 | awk '{print $NF}' |awk -F"/" '{print $1}' | xargs kill -9
 sh /data/scripts/alive.sh
+
