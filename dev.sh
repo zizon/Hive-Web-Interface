375275825dc5bf358082cde4b0df8c87418dae1a
kevin <zhizhong.qiu@happyelements.com>
2012-05-03 10:30:36 +0800

    sync

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 git pull;mvn clean package;
-hadoop jar "target/`ls target/ |grep jar`" com.happyelements.hive.web.Starter /home/hadoop/hehwi /home/hadoop/hive/logs 9888 /main.html
+hadoop jar "target/`ls target/ |grep jar`" com.happyelements.hive.web.Starter /home/hadoop/hehwi /home/hadoop/hehwi 9888 /main.html
